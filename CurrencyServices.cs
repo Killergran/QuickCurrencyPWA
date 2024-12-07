0e0856a0a8362cd2a630c8631ac5649d5ed457a2
Jakop Kilegran <Killergran@gmail.com>
2024-12-07 20:06:48 +0100

    Add project files.

---
+++ CurrencyServices.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace QuickCurrencyPWA
+{
+    public class CurrencyService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CurrencyService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ApiResponse> GetCurrencyRatesAsync(string apiKey, string currency)
+        {
+            var url = $"https://v6.exchangerate-api.com/v6/{apiKey}/latest/{currency}";
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                var jsonSerialized = JsonSerializer.Deserialize<ApiResponse>(json, options);
+                return jsonSerialized;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ RequestModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+    public class ApiResponse
+    {
+        public string Result { get; set; }
+        public string Documentation { get; set; }
+        public string Terms_Of_Use { get; set; }
+        public long Time_Last_Update_Unix { get; set; }
+        public string Time_Last_Update_Utc { get; set; }
+        public long Time_Next_Update_Unix { get; set; }
+        public string Time_Next_Update_Utc { get; set; }
+        public string Base_Code { get; set; }
+        public Dictionary<string, decimal> Conversion_Rates { get; set; }
+    }
+    class RequestModel
+    {
+    }
