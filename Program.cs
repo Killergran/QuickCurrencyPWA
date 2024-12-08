b793044470e4a7eb2f0f716454410bfcf54ade3e
Jakop Kilegran <Killergran@gmail.com>
2024-12-08 20:11:25 +0100

    Add LocalStorage to be able to run offline.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 using QuickCurrencyPWA;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,5 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new CurrencyService(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }));
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
