77a1a98786cdb1a336b3a0d07caa6b943b0cbde9
Jakop Kilegran <Killergran@gmail.com>
2024-12-13 21:59:50 +0100

    Fix inversion of SEK to be proper.

--- CurrencyExchange.cs
+++ CurrencyExchange.cs
@@ -11,5 +11,31 @@ namespace QuickCurrencyPWA
         public string FromCurrency { get; set; }
         public string ToCurrency { get; set; }
         public decimal ExchangeRate { get; set; }
+        
+        /// <summary>
+        /// Inverts the CurrencyExchangeRate object. Use with CAUTION!
+        /// </summary>
+        public void Invert()
+        {
+            string temp = FromCurrency;
+            FromCurrency = ToCurrency;
+            ToCurrency = temp;
+            ExchangeRate = 1 / ExchangeRate;
+        }
+
+        /// <summary>
+        /// Returns an inverted copy of the current exchange rate
+        /// </summary>
+        /// <returns>This exchangerate inverted</returns>
+        public CurrencyExchangeClass Inverted()
+        {
+            return new CurrencyExchangeClass
+            {
+                ExchangeRate = 1 / this.ExchangeRate,
+                FromCurrency = this.ToCurrency,
+                ToCurrency = this.FromCurrency
+            };
+        }
     }
+    
 }
